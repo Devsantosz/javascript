3bd16b439a71ca37e7ad4972149214590fd3328f
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-07-03 17:24:36 -0300

    Exerc JS - Devsantosz

--- Exerc JS/aula12exec/exe014/script.js
+++ Exerc JS/aula12exec/exe014/script.js
@@ -1,22 +1,22 @@
 function carregar(){
 
 var msg = window.document.getElementById('msg')
-var foto = window.document.getElementById('foto')
+var img = window.document.getElementById('imagem')
 var data = new Date()
-//var hora = data.getHours()//
-var hora = 22
-msg.innerHTML = `Agora sao ${hora} horas.`
-
-
-
-if (hora >= 0 && hora < 12) {
-    //Bom Dia!!
-    img.src = 'fotomanha.png'
-}else  if (hora >= 13 && hora < 18) {
-    //Boa tarde
-    img.src = 'fototarde.png'
-}else { 
-    //Boa noite
-    img.src = 'fotonoite.png'
-}
+var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12) {
+        //Bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#735443'
+    }else  if (hora >= 13 && hora <= 18) {
+        //Boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background = '#733410'
+    }else { 
+        //Boa noite!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#0A2326'
+    }
 }
