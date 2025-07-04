a97ec171ca1e9667b8650a9d6c0bdfeb91bc9f7f
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-07-04 18:34:02 -0300

    Exemplos de DO e WHILE - Devsantosz

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,12 +1,18 @@
+do{
+    console.log("Tudo bem?")
+    c++
+}while(c <= 6)
+// Exemplo de loop do-while
+
 var c = 1
 while (c <= 6) {
     console.log("Tudo bem?")
     c++
 }
+// Exemplo de loop while
+
 
 /*
-console.log("Tudo bem?")
-console.log("Tudo bem?")
 console.log("Tudo bem?")
 console.log("Tudo bem?")    
 console.log("Tudo bem?")
