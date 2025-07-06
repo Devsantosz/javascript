8e5d5634fa6a3b4310b7f69bd1e4a3ae6a59857b
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-07-06 13:29:03 -0300

    Aprendendo a fazer(Exerc de contagem) - Devsantosz

---
+++ aula14exe/ex016/script.js
@@ -0,0 +1,11 @@
+function contar(){
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')    
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[Erro!] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+    }
+}
