475ec2f039dbddc1e79329da22d01e9f2b1f6867
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2025-07-15 18:06:28 -0300

    Exercicio de analisar numeros - Devsantosz

---
+++ aula16ex/modelo/script.js
@@ -0,0 +1,30 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('diuv#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, lista){
+  if(lista.indexOf(n) != -1){
+    return true
+  }else{
+    return false
+  }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && ! inLista(num.value, valores)){
+        window.alert('Tudo ok!')
+    }
+    else {
+        alert('Valor inválido ou já encontrado na lista.')
+    }
+
+}
